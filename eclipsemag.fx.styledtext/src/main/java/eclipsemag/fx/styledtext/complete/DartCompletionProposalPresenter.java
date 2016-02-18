18abbaba1f58161c5080d4a8ab8cac2a1324799d
Tom Schindl <tom.schindl@bestsolution.at>
2016-02-18 12:06:18 +0100

    final fixes

---
+++ eclipsemag.fx.styledtext/src/main/java/eclipsemag/fx/styledtext/complete/DartCompletionProposalPresenter.java
@@ -0,0 +1,17 @@
+package eclipsemag.fx.styledtext.complete;
+
+import org.eclipse.fx.code.editor.fx.services.CompletionProposalPresenter;
+import org.eclipse.fx.code.editor.services.CompletionProposal;
+import org.eclipse.fx.text.ui.contentassist.ICompletionProposal;
+import org.eclipse.fx.ui.controls.styledtext.TextSelection;
+import org.eclipse.jface.text.IDocument;
+
+import javafx.scene.Node;
+
+public class DartCompletionProposalPresenter implements CompletionProposalPresenter {
+
+	@Override
+	public ICompletionProposal createProposal(CompletionProposal proposal) {
+		return null;
+	}
+}
